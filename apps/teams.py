e5680ecfb23d1fe026e7cea09d8114800c7e3ead
Arvin Poddar <arvin.poddar@gmail.com>
2022-10-02 16:53:53 -0400

    Update endpoint specifications

--- apps/teams.py
+++ apps/teams.py
@@ -22,13 +22,25 @@ GET /teams -> Get all teams for the user
 
 POST /teams -> Create a new team (I'll give you a name and an array of users)
 Each user will have an email and a role. You also need to add the user who created
-this team as an owner.
+this team as an owner. The post body will look something like this:
+{
+    name: '',
+    users: [list of user objects, with system IDs]
+}
+This endpoint will need to make sure every user id exists.
+
+GET /teams/:team-id -> Return the team object (just name and users right now). 
+Make sure that the requesting user is in the team.
 
 PUT /teams/:team-id -> First, make sure the ID of the current user is that of an owner/admin
 in the requested team. This is to update whatever is in the team. We can use this endpoint to
 edit team name, or edit which users are inside of the team and their roles.
 
 DELETE /teams/:team-id -> Delete the entire team.
+
+DELETE /teams/:team-id/user/:user-id -> Deletes a user from a particular team. Do not delete from our system.
+First, verify that the requesting user is an admin/owner of the team. Then, make sure the user to delete 
+is in the team.
 """
 
 """
--- apps/users.py
+++ apps/users.py
@@ -3,6 +3,8 @@ TODO: CREATE FOLLOWING ENDPOINTS:
 
 THIS IS FOR THE FRONTEND:
 {
+    id: '',
+    email: ''
     first_name: 'Team Name',
     last_name: '',
     bio: 'Hi I work at Google',
@@ -19,6 +21,15 @@ GET /user -> Return the information about the requesting user.
 GET /user/verify?email={email@test.com} -> Return whether an account exists with the given email address.
 Return the full user object if yes, otherwise give a 404 response error
 
-POST /user -> Create the user
-PUT /user -> Update the user
+POST /user -> Create the user. I'll give you this information in the POST BODY. First, check that the
+email doesn't already exist in our system.
+{
+    email: ''
+    first_name: 'Team Name',
+    last_name: '',
+    bio: 'Hi I work at Google'
+}
+
+
+PUT /user -> Update the user. You'll get the same body as the POST request
 """
