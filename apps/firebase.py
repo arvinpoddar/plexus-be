c7fe57ec2196829df9c7ddc6f18561d648f64ec2
Arvin Poddar <arvin.poddar@gmail.com>
2022-12-01 16:56:52 -0500

    Move firebase credentials to root

--- apps/firebase.py
+++ apps/firebase.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
 
-file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"firebase_creds.json")
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"../firebase_creds.json")
 cred = credentials.Certificate(file_path)
 
 db_app = firebase_admin.initialize_app(cred)
